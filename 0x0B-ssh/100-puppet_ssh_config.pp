fd599a10d2a217db4019495ab9b0844e0787aef9
tluciad <tluciad@gmail.com>
2022-08-23 11:16:55 -0500

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Puppet to make changes to our configuration file
+file_line {'PasswordAuthentication':
+    ensure  => 'present'
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    replace => true
+}
+file_line {'Configuration ssh':
+    ensure  => 'present',
+    path    => '/etc/ssh/ssh_config',
+    line    => 'IdentityFile ~/.ssh/school',
+    replace => true
+}
