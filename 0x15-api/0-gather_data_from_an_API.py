f92d18427c54480d488e17c3369facd7bc01268f
tluciad <tluciad@gmail.com>
2022-09-09 11:22:23 -0500

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,26 +7,26 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """module to returns information about his/her TODO list"""
+
     userId = (argv[1])
-    response_user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(userId)).json()
-    response_todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos".format(userId)).json()
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+    respUser = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    respTodos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos".format(userId))
+
+    taskcompleted = 0
+    totalnumtask = 0
+    name = respUser.json().get('name')
     Title = []
-    EMPLOYEE_NAME = response_user['name']
 
-    for items in response_todos:
-        U_id = items['userId']
-        if (U_id == int(userId)):
-            TOTAL_NUMBER_OF_TASKS += 1
-            if (items['completed'] is True):
-                Title.append(items['title'])
-                NUMBER_OF_DONE_TASKS += 1
+    for item in respTodos.json():
+        if item.get('userId') == int(userId):
+            totalnumtask += 1
+            if item.get('completed'):
+                taskcompleted += 1
 
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks\
-            ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-    for title in Title:
-        print(f"\t{title}")
+    print("Employee {} is done with tasks({}/{}):".format(name,
+          taskcompleted, totalnumtask))
+
+    print('\n'.join(["\t " + item.get('title') for item in respTodos.json()
+          if item.get('userId') == int(userId) and item.get('completed')]))
