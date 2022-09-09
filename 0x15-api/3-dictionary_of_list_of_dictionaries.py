18d8a42a8652a8e72ca11bad25efa9ec8880dd7b
tluciad <tluciad@gmail.com>
2022-09-09 16:50:24 -0500

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""extend your Python script to export data in the JSON format."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+
+    respUser = requests.get(
+        "https://jsonplaceholder.typicode.com/users").json()
+    respTodos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
+
+    NewDictionary = {}
+    for user in respUser:
+        tasks = []
+        for item in respTodos:
+            if user.get('id') == item.get('userId'):
+                tskDictionary = {"username": user.get('username'),
+                                 "task": item.get('title'),
+                                 "completed": item.get('completed')}
+                tasks.append(tskDictionary)
+                print(tasks)
+        NewDictionary[user.get('id')] = tasks
+        with open("todo_all_employees.json", 'w') as Jfile:
+            json.dump(NewDictionary, Jfile)
